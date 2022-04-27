3df996caeb8d8b21f21cc888e05346079d492c01
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2022-04-27 00:48:50 -0500

    added data class for profile saving

--- src/FinalProj/utils/YamlParser.java
+++ src/FinalProj/utils/YamlParser.java
@@ -3,6 +3,12 @@ package FinalProj.utils;
 public class YamlParser {
     private String path;
 
+    public class Data {
+        public String profile;
+        public int guilt = 0;
+        public int resolve = 0;
+    }
+
     YamlParser(String path) {
         this.path = path;
     }
